69e1271965ac9f500bcdee3ac0bf55c2ecca2b08
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-30 10:15:49 -0400

    Initial commit

---
+++ src/main/java/Prisoner.java
@@ -0,0 +1,15 @@
+
+public class Prisoner {
+
+    public String n;
+    public double h;
+    public int s;
+
+    public Prisoner(String n, double h, int s){
+        this.n=n;
+        this.h=h;
+        this.s=s;
+    }
+
+
+}
---
+++ src/test/java/PrisonerTest.java
@@ -0,0 +1,10 @@
+
+public class PrisonerTest {
+    public static void main(String[] args){
+        Prisoner p01 = new Prisoner("Bubba",2.08,4);
+        Prisoner p02 = new Prisoner("Twitch",1.73,3);
+
+
+
+    }
+}
